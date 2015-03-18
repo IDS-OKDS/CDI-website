89df654c884f9333a01dfb8e3280483d9ad04bed
Joe McPherson <j.m.p.mcpherson@gmail.com>
2015-03-18 14:17:17 +0000

    fix toplevel menu for dropdowns

--- sites/all/themes/cdi/template.php
+++ sites/all/themes/cdi/template.php
@@ -5,4 +5,9 @@
 function cdi_preprocess_page(&$variables) {
   // Clears the copyright message variable from Radix default
   $variables['copyright'] = '';
+  // Get the entire main menu tree
+  $main_menu_tree = menu_tree_all_data('main-menu');
+  // Add the rendered output to the $main_menu_expanded variable
+  $menu_output = menu_tree_output($main_menu_tree);
+  $variables['main_menu_expanded'] = $menu_output;
 }

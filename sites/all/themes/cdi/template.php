6f2e15cf31092f248c1789ae6adba376265cb1e5
Joe McPherson <j.m.p.mcpherson@gmail.com>
2015-03-27 13:47:46 +0000

    breadcrumbs for certain types

--- sites/all/themes/cdi/template.php
+++ sites/all/themes/cdi/template.php
@@ -5,4 +5,17 @@
 function cdi_preprocess_page(&$variables) {
   // Clears the copyright message variable from Radix default
   $variables['copyright'] = '';
+  $show_breadcrumb = false;
+  if(isset($variables['node'])){
+  	if(in_array($variables['node']->type, array('panopoly_news_article', 'publication', 'event', 'person', 'blog', 'partner', 'project'))){
+  		$show_breadcrumb = true;
+  	}
+  }
+  if(arg(0) == 'admin'){
+  	$show_breadcrumb = true;
+  }
+  if(!$show_breadcrumb){
+  	$variables['breadcrumb'] = '';
+  }
+  
 }

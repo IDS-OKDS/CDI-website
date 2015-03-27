0154f241b518fc9069edef35a080e699ee697318
Joe McPherson <j.m.p.mcpherson@gmail.com>
2015-03-27 15:26:33 +0000

    feedback javascript

--- sites/all/themes/cdi/templates/page/page.tpl.php
+++ sites/all/themes/cdi/templates/page/page.tpl.php
@@ -102,3 +102,26 @@
     <div id="footer-region" class="container"><?php print render($page['footer']); ?> </div>
   <?php endif; ?>
 </footer>
+
+
+<script>
+       var _vengage = _vengage || [];
+              (function(){
+              var a, b, c;
+              a = function (f) {
+              return function () {
+              _vengage.push([f].concat(Array.prototype.slice.call(arguments, 0)));
+            };
+          };
+          b = ['load', 'addRule', 'addVariable', 'getURLParam', 'addRuleByParam', 'addVariableByParam', 'trackAction', 'submitFeedback', 'submitResponse', 'close', 'minimize', 'openModal', 'helpers'];
+          for (c = 0; c < b.length; c++) {
+          _vengage[b[c]] = a(b[c]);
+        }
+        var t = document.createElement('script'),
+        s = document.getElementsByTagName('script')[0];
+        t.async = true;
+        t.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://s3.amazonaws.com/vetrack/init.min.js';
+        s.parentNode.insertBefore(t, s);
+        _vengage.push(['pubkey', '2a12c615-3b8b-411e-b434-ad6dddfa5b52']);
+      })();
+</script>

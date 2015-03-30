74b08e9f3122d64835adaac8006c95a4f3518955
Joe McPherson <j.m.p.mcpherson@gmail.com>
2015-03-30 16:29:29 +0100

    link field theming for publication doc links

--- sites/all/themes/cdi/template.php
+++ sites/all/themes/cdi/template.php
@@ -19,3 +19,57 @@ function cdi_preprocess_page(&$variables) {
   }
   
 }
+
+
+/**
+ * Implements theme_field().
+ */
+
+function cdi_field__field_other_resource($variables) {
+	return cdi_field_link($variables);
+}
+
+function cdi_field__field_opendocs_link($variables) { 
+	return cdi_field_link($variables);
+}
+
+function cdi_field_link($variables) {
+	/* Only show link fields that actually have a URL (Because we defult the title and 
+	 * so it tries to display without one as URL needs to be otional to save the node with a default title!!) */
+	
+  $has_a_link_to_show = FALSE;
+
+  foreach ($variables ['items'] as $delta => $item) {
+  	if($item['#element']['url']){
+  		$has_a_link_to_show = TRUE;
+  	}
+  }
+  
+  
+  $output = '';
+  
+  if($has_a_link_to_show){
+	  // Render the label, if it's not hidden.
+	  if (!$variables ['label_hidden']) {
+	    $output .= '<div class="field-label"' . $variables ['title_attributes'] . '>' . $variables ['label'] . ':&nbsp;</div>';
+	  }
+	
+	  // Render the items.
+	  $output .= '<div class="field-items"' . $variables ['content_attributes'] . '>';
+	  foreach ($variables ['items'] as $delta => $item) {
+	  	if($item['#element']['url']){
+		    $classes = 'field-item ' . ($delta % 2 ? 'odd' : 'even');
+		    $output .= '<div class="' . $classes . '"' . $variables ['item_attributes'][$delta] . '>' . drupal_render($item) . '</div>';
+	  	}
+	  }
+	  $output .= '</div>';
+	
+	  // Render the top-level DIV.
+	  $output = '<div class="' . $variables ['classes'] . '"' . $variables ['attributes'] . '>' . $output . '</div>';
+  }
+
+  return $output . ' ';
+}
+
+
+
